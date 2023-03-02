42dc8f57c93ee33b53d5a4ff16bf021355a6e3b6
sandeshjadhav5 <jadhavs932@gmail.com>
2023-03-02 13:12:09 +0530

    added userRoutes

---
+++ routes/User.routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const { UserModel } = require("../models/User.model");
+
+const userRouter = express.Router();
+
+userRouter.post("/", async (req, res) => {
+  const { name, score, difficulty } = req.body;
+  try {
+    const user = new UserModel({ name, score, difficulty });
+    await user.save();
+    res.send("User Added");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = {
+  userRouter,
+};
